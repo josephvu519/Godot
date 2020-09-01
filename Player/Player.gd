c3bd32f458e9e24af42fa1a833d5c36c85bd5e45
josephvu519 <33297434+josephvu519@users.noreply.github.com>
2020-09-01 18:49:38 -0400

    Linked movement to character
    Now properly performs running animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,23 +2,28 @@ extends KinematicBody2D
 
 const FRICTION = 1
 const ACCEL = 1
-const MAX_SPEED = 100
+const MAX_SPEED = 50
 
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var inputVector = Vector2.ZERO;
 	inputVector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left");
 	inputVector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up");
+	inputVector = inputVector.normalized()
 	
 	if (inputVector != Vector2.ZERO):
-		animationPlayer.play("RunRight")
-		velocity += inputVector.normalized() * ACCEL * delta
+		animationTree.set("parameters/Idle/blend_position", inputVector)
+		animationTree.set("parameters/Run/blend_position", inputVector)
+		animationState.travel("Run")
+		velocity += inputVector * ACCEL * delta
 		velocity = velocity.clamped(MAX_SPEED * delta)
 	else:
-		animationPlayer.play("IdleRight")
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta);
 	print(velocity)
 	
